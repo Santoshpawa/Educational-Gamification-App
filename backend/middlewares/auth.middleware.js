ad009d8208dec22b44e1d5222e7ebb9533a3e5b3
Santosh Singh Pawar <textingsantosh@gmail.com>
2025-11-28 19:34:23 +0530

    debugging undefined request header error

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -4,8 +4,9 @@ import { userModel } from "../models/user.model.js";
 export async function verifyJWT(req, res, next) {
   try {
     console.log("Inside verifyJWT function");
-    console.log("request cookies", req);
-   // console.log("request header authorization", req.header("Authorization"));
+    console.log("request cookies", req.cookies?.accessToken);
+
+    // console.log("request header authorization", req.header("Authorization"));
     var token =
       req.cookies?.accessToken || req.header("Authorization").split(" ")[1];
 
