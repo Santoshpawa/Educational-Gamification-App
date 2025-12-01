61223f0f90751f91a4739f8a3f634ce34df186b0
Santosh Singh Pawar <textingsantosh@gmail.com>
2025-12-01 18:37:04 +0530

    mismatch of api for development and hoisting

--- frontend/src/utils/backendAPI.js
+++ frontend/src/utils/backendAPI.js
@@ -1,4 +1,4 @@
-const onlineAPI = "https://educational-gamification-app-c8wc.onrender.com";
-const baseAPI = "http://localhost:3000";
+const baseAPI = "https://educational-gamification-app-c8wc.onrender.com";
+const localAPI = "http://localhost:3000";
 
 export { baseAPI };
